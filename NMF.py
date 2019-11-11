d9cfcabf15ee1192f128ea21f9b402d843bee1ed
sergiofilho07 <sergiofilhoce07@gmail.com>
2019-11-11 10:48:01 -0300

    Update NMF.py
    Add KL divergence function

--- NMF.py
+++ NMF.py
@@ -136,3 +136,9 @@ def nmf_kl(X, k, num_iter, init_W=None, init_H=None, print_enabled=False):
             print("iteration " + str(n + 1) + ": " + str(frob_norm))
 
     return W, H
+
+def KL(a, b):
+    a = np.asarray(a, dtype=np.float)
+    b = np.asarray(b, dtype=np.float)
+
+    return np.sum(np.where(a != 0, a * np.log(a / b), 0))
